65f1bc869d46b967e690926a30eb45fc9125ff83
John Heintz <john@gistlabs.com>
2021-05-17 22:59:17 -0500

    Initial structure and one version dispatch working now

--- versionedfunction/versionedfunction.py
+++ versionedfunction/versionedfunction.py
@@ -3,7 +3,64 @@
 
 """
 
+
+A bit of naming: vfunc is a versionedfunction, and vfuncv is a version of a versionedfunction
 """
 
-def versionedfunction(func):
-    return func
+versions = [] # list of versions to use for versionedfunctions, global context
+
+def versionedfunction(vfunc):
+    versionInfo = VersionInfo(vfunc)# main data structure to hold state for versionedfunctions
+
+    def version(vfuncv):
+        """
+
+        :param vfuncv: Functions to be versioned
+        :return:
+        """
+        versionName = _find_version_name(vfunc.__name__, vfuncv.__name__)
+        versionInfo[versionName] = vfuncv
+
+        return vfuncv
+
+    def vfunc_wrapper(*args, **kwargs):
+        if versions:
+            v = versions[0]
+            f = versionInfo[v]
+        else:
+            f = vfunc
+        return f(*args, **kwargs)
+
+    vfunc_wrapper.versionInfo = versionInfo
+    vfunc_wrapper.version = version
+
+    return vfunc_wrapper
+
+class VersionInfo():
+    """
+    This data structure is used for each versionedfunction and connects the initial and each version together
+    """
+    def __init__(self, vfunc):
+        self.vfunc = vfunc
+        self.versions = {}
+
+    @property
+    def name(self):
+        return self.vfunc.__name__
+
+    def __getitem__(self, item):
+        return self.versions[item]
+
+    def __setitem__(self, key, value):
+        self.versions[key] = value
+
+def _find_version_name(vfuncName, vfuncvName):
+    """
+    Remove the base versionedfunction name and left strip - characters
+
+    :param vfuncName: A versionedfunction name (string)
+    :param vfuncvName: A function that is a version of a versionedfunction (name, string again)
+    :return:
+    """
+    assert vfuncvName.startswith(vfuncName)
+    return vfuncvName[len(vfuncName):].lstrip('_')
--- versionedfunction/versionedfunction_test.py
+++ versionedfunction/versionedfunction_test.py
@@ -5,12 +5,36 @@
 
 """
 
-from versionedfunction import versionedfunction
+from versionedfunction import versionedfunction, versions
+
 
 
 @versionedfunction
 def fooAlgo():
-    return 1
+    return 0
 
 def test_decorator():
-    assert fooAlgo() == 1
+    assert fooAlgo() == 0
+
+@fooAlgo.version
+def fooAlgo_v1():
+    return 1
+
+def test_fooAlgo_version_annotation():
+    return fooAlgo_v1 == 1
+
+def test_fooAlgo_v1_in_versions():
+    assert fooAlgo.versionInfo is not None
+    assert fooAlgo.versionInfo['v1'] == fooAlgo_v1
+
+@fooAlgo.version
+def fooAlgo_v2():
+    return 2
+
+def test_fooAlgo_v2_in_versions():
+    assert fooAlgo.versionInfo['v2'] == fooAlgo_v2
+
+def test_call_v2():
+    versions.clear()
+    versions.append("v2")
+    assert fooAlgo() == 2
